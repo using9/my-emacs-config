1c82951c68d09a74702534af522284b8f74100f7
using9 <using9@126.com>
2023-01-16 10:52:16 +0800

    Update init-server.el

--- lisp/init-server.el
+++ lisp/init-server.el
@@ -1,14 +1,3 @@
-;;
-;;
-;;
-;; -- server mode config.
-;;(server-force-delete)
-;; HKEY_CLASSES_ROOT\*\shell\Emacs\command
-;;"d:\soft\emacs28\bin\emacsclientw.exe"  --no-wait --quiet
-;;   --server-file="d:\home\server\server_file"
-;;   --alternate-editor="d:\soft\emacs29\bin\runemacs.exe" "%0"
-
-;;(add-hook 'after-init-mode 'server-mode)
 (server-mode)
 (setq server-auth-dir "~\\server")
 (setq server-name "server_file")
