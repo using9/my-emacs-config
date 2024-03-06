c6f72c02d6d9d0fd08253ef0553c77ee198ed19a
using9 <using9@126.com>
2024-03-06 14:39:58 +0800

    replace elpa for git,and replace lisp to modes

--- elpa/pyim-basedict/pyim-basedict-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; pyim-basedict-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "pyim-basedict" "pyim-basedict.el" (0 0 0 0))
-;;; Generated autoloads from pyim-basedict.el
-
-(autoload 'pyim-basedict-enable "pyim-basedict" "\
-Add pyim-basedict.pyim file to `pyim-extra-dicts'." t nil)
-
-;;;***
-
-;;;### (autoloads nil "pyim-basedict-utils" "pyim-basedict-utils.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from pyim-basedict-utils.el
-
-(register-definition-prefixes "pyim-basedict-utils" '("pyim-basedict-"))
-
-;;;***
-
-(provide 'pyim-basedict-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; pyim-basedict-autoloads.el ends here
--- lisp/init-yasnippet.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'load-path
-              "~/.emacs.d/elpa/yasnippet")
-(require 'yasnippet)
-(yas-global-mode 1)
-
-(provide 'init-yasnippet)
